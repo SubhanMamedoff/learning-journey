9662a2f29de2e3e70da490eeb73e7c1360e528f2
SubhanMamedoff <subhanmamedoff010@gmail.com>
2025-09-02 13:48:19 +0400

    Add Problem Solution - recover.c

---
+++ cs50x/pset4/recover/recover.c
@@ -0,0 +1,57 @@
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+    // Accept a single command-line argument
+    if (argc != 2)
+    {
+        printf("Usage: ./recover FILE\n");
+        return 1;
+    }
+
+    // Open the memory card
+    FILE *card = fopen(argv[1], "r");
+    FILE *img = NULL;
+
+    // Create a buffer for a block of data
+
+    uint8_t buffer[512];
+    int count = 0;
+    char filename[8];
+    if (card == NULL)
+    {
+        printf("Could not open file.\n");
+        return 1;
+    }
+
+    // While there's still data left to read from the memory card
+    while (fread(buffer, 1, 512, card) == 512)
+    {
+        if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff &&
+            (buffer[3] & 0xf0) == 0xe0)
+        {
+            if (count != 0)
+            {
+                fclose(img);
+            }
+            sprintf(filename, "%03i.jpg", count);
+            img = fopen(filename, "w");
+            fwrite(buffer, 512, 1, img);
+            count++;
+        }
+        else
+        {
+            if (img != NULL)
+            {
+                fwrite(buffer, 512, 1, img);
+            }
+        }
+    }
+    fclose(card);
+    if (img != NULL)
+    {
+        fclose(img);
+    }
+}

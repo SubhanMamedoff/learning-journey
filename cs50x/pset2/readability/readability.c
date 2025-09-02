c2a5efc86e3f8ff445b1d4dc5c43b8377eb149e3
SubhanMamedoff <subhanmamedoff010@gmail.com>
2025-09-02 09:28:58 +0400

    Add Problem Solution - readability.c

---
+++ cs50x/pset2/readability/readability.c
@@ -0,0 +1,73 @@
+#include <cs50.h>
+#include <ctype.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+int count_letters(string text, int length);
+int count_words(string text, int length);
+int count_sentences(string text, int length);
+int main(void)
+{
+    string text = get_string("Text: ");
+    int length = strlen(text);
+    int letters = count_letters(text, length);
+    int words = count_words(text, length);
+    int sentences = count_sentences(text, length);
+    float L = (float) letters / words * 100;
+    float S = (float) sentences / words * 100;
+    float index = 0.0588 * L - 0.296 * S - 15.8;
+    int grade = round(index);
+
+    if (grade < 1)
+    {
+        printf("Before Grade 1\n");
+    }
+    else if (grade >= 16)
+    {
+        printf("Grade 16+\n");
+    }
+    else
+    {
+        printf("Grade %i\n", grade);
+    }
+}
+
+int count_letters(string text, int length)
+{
+
+    int letters = 0;
+    for (int i = 0; i < length; i++)
+    {
+        if (isalpha(text[i]))
+        {
+            letters++;
+        }
+    }
+    return letters;
+}
+
+int count_words(string text, int length)
+{
+    int words = 1;
+    for (int i = 0; i < length; i++)
+    {
+        if (text[i] == ' ')
+        {
+            words++;
+        }
+    }
+    return words;
+}
+
+int count_sentences(string text, int length)
+{
+    int sentences = 0;
+    for (int i = 0; i < length; i++)
+    {
+        if (text[i] == '.' || text[i] == '!' || text[i] == '?')
+        {
+            sentences++;
+        }
+    }
+    return sentences;
+}
